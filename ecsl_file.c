58389fe1dbf866e80d21b8f79443a402cb93ec04
DanielNanto <nanto.daniel@gmail.com>
2022-08-11 15:36:42 -0500

    Defined escl_file_set_line()

--- ecsl_file.c
+++ ecsl_file.c
@@ -1,1 +1,18 @@
 #include "ecsl_file.h"
+
+int escl_file_set_line(FILE* file, int dst_line_n)
+{
+  int errors = 0;
+  fseek(file, 0L, SEEK_SET);
+  char c = '0';
+  int line_counter = 0;
+  while(line_counter < dst_line_n && c != EOF)
+  {
+    c = fgetc(file);
+    if(c == '\n')
+    {
+      ++line_counter;
+    }
+  }
+  return errors;
+}
