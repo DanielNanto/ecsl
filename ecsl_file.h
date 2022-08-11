88810b05a70c9ee1b014fb345e3b3cbf7e5e318a
DanielNanto <nanto.daniel@gmail.com>
2022-08-11 15:35:04 -0500

    Documented escl_file_set_line()

--- ecsl_file.h
+++ ecsl_file.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 
+//! \brief Seek to the start of a given line number within the provided file.
+//! \param file The file stream to seek into.
+//! \param dst_line_n The desired line number.
+//! \returns 0 if successful, 1 if unable to seek to desired line.
 extern int escl_file_set_line(FILE* file, int dst_line_n);
 
 #endif
