38626ee7899dd924f8b32a1c66c0d5f3ba90b6e2
DanielNanto <nanto.daniel@gmail.com>
2022-08-16 17:54:37 -0500

    Declared ecsl_file_get_line_str()

--- ecsl_file.h
+++ ecsl_file.h
@@ -4,15 +4,21 @@
 #include <stdio.h>
 
 //! \brief Seek to the start of a given line number within the provided file.
-//! \param file The file stream to seek into.
+//! \param file The FILE* to seek into.
 //! \param line_n The line number requested to seek to.
-//! \returns 0 if successful, 1 if unable to seek to desired line.
+//! \returns 0 if successful, 1 if unable to seek to the desired line.
 extern int ecsl_file_set_line(FILE* file, int line_n);
 
 //! \brief Find the character length of a given line within the provided file.
-//! \param file The FILE* to evaluate
-//! \param line_n The desired line number.
-//! \returns length of desired line
+//! \param file The FILE* to evaluate.
+//! \param line_n The line number of a FILE* to be evaluated.
+//! \returns the length of desired line.
 extern int ecsl_file_get_line_length(FILE* file, int line_n);
+
+//! \brief Extract a string from the given line of a provided file.
+//! \param file The FILE* to evaluate.
+//! \param line_n The line number of a FILE* to be read.
+//! \returns the extracted string.
+extern char* ecsl_file_get_line_str(FILE* file, int line_n);
 
 #endif
