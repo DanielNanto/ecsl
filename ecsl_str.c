b2a5f2a281a512086b0697614ee206098302b587
DanielNanto <nanto.daniel@gmail.com>
2022-08-22 10:49:22 -0500

    Defined str_array_length()

--- ecsl_str.c
+++ ecsl_str.c
@@ -1,1 +1,22 @@
 #include "ecsl_file.h"
+
+int str_array_length(char* str)
+{
+  // Note: expected str format: "[a,b,c]"
+  int str_length = strlen(str);
+  int array_length = 0;
+  // If there is at least one number between '[' and ']'
+  if (str_length > 2)
+  {
+    array_length = 1;
+    int i;
+    for (i = 1; i < str_length; i++)
+    {
+      if (str[i] == ',')
+      {
+        ++array_length;
+      }
+    }
+  }
+  return array_length;
+}
