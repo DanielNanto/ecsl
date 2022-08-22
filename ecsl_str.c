41ecce388bb1590fc8694190a07c0a3f011cd6bb
DanielNanto <nanto.daniel@gmail.com>
2022-08-22 10:54:23 -0500

    Defined str_remove_spaces()

--- ecsl_str.c
+++ ecsl_str.c
@@ -5,8 +5,7 @@ int str_array_length(char* str)
   // Note: expected str format: "[a,b,c]"
   int str_length = strlen(str);
   int array_length = 0;
-  // If there is at least one number between '[' and ']'
-  if (str_length > 2)
+  if (str_length > 2) // If there is at least one number between '[' and ']'
   {
     array_length = 1;
     int i;
@@ -19,4 +18,25 @@ int str_array_length(char* str)
     }
   }
   return array_length;
+}
+
+char* str_remove_spaces(char* str)
+{
+  char* str_1 = NULL;
+  int str_length = strlen(str);
+  str_1 = (char*)calloc(sizeof(char), str_length);
+  int i = 0, j = 0;
+  for (i = 0; i < str_length; i++)
+  {
+    if (str[i] != ' ')
+    {
+      str_1[j] = str[i];
+      ++j;
+    }
+  }
+  if (j != str_length)
+  {
+    str_1 = (char*)realloc(str_1, j * sizeof(char)); // Trimming excess memory
+  }
+  return str_1;
 }
