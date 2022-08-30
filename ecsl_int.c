20d13366fc342a7d0684f455fa0ee12687c945ea
DanielNanto <nanto.daniel@gmail.com>
2022-08-30 08:46:17 -0500

    Defined int_get_length()

--- ecsl_int.c
+++ ecsl_int.c
@@ -1,1 +1,14 @@
 #include "ecsl_int.h"
+
+int int_get_length(int n)
+{
+  int length = 0;
+  int n_tmp = n % (int)pow(10, length);
+  while (n_tmp != n)
+  {
+    ++length;
+    n_tmp = n % (int)(pow(10, length));
+  }
+  length += (n <= 0) ? 1 : 0;
+  return length;
+}
