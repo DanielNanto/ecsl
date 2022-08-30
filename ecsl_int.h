12dc31b4be3f7b7fd579c9ee26d7d29bc20728f0
DanielNanto <nanto.daniel@gmail.com>
2022-08-30 08:52:39 -0500

    Added function description to int_get_length()

--- ecsl_int.h
+++ ecsl_int.h
@@ -3,6 +3,10 @@
 
 #include <math.h>
 
+//! \brief Count the digits of a given integer. If the integer is negative, one
+//! \brief one additional digit is included for the sign.
+//! \param n The int to be evaluated.
+//! \returns The number of digits within the provided integer.
 extern int int_get_length(int n);
 
 #endif
