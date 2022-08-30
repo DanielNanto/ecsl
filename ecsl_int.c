bfec94bd28a91a8e6bbd1fd42a13b9f2d539dabf
DanielNanto <nanto.daniel@gmail.com>
2022-08-30 12:02:28 -0500

    Added positive sign check to int_get_digit()

--- ecsl_int.c
+++ ecsl_int.c
@@ -22,11 +22,12 @@ int int_get_digit(int n, int digit_target)
     int digit_ltr = (int)pow(10, digit_total - digit_target + 1);
     // Truncate numbers before and after desired digit.
     value = (n % digit_ltr) / (digit_ltr / 10);
+    value *= (value < 0) ? -1 : 1;
   }
   else
   {
-    value = -11;
     printf("Error: Unable to read digit %d int_get_digit()\n", digit_target);
+    value = -11;
   }
   return value;
 }
