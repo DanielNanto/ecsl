518ac3c1c5417476e4ef2350f261085cd3c3ea5e
DanielNanto <nanto.daniel@gmail.com>
2022-08-30 11:32:56 -0500

    Defined int_get_digit()

--- ecsl_int.c
+++ ecsl_int.c
@@ -11,3 +11,22 @@ int int_get_digit_total(int n)
   }
   return length;
 }
+
+int int_get_digit(int n, int digit_target)
+{
+  int value = 0;
+  int digit_total = int_get_digit_total(n);
+  if (digit_target > 0 && digit_target <= digit_total)
+  {
+    // Convert digit_target from right->left to left->right (ltr).
+    int digit_ltr = (int)pow(10, digit_total - digit_target + 1);
+    // Truncate numbers before and after desired digit.
+    value = (n % digit_ltr) / (digit_ltr / 10);
+  }
+  else
+  {
+    value = -11;
+    printf("Error: Unable to read digit %d int_get_digit()\n", digit_target);
+  }
+  return value;
+}
