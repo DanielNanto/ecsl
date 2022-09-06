cb61c95a5f26a154db2de58d4047077e50db21f7
DanielNanto <nanto.daniel@gmail.com>
2022-09-06 11:20:44 -0500

    Temporarily delaying certain functions

--- ecsl_file.h
+++ ecsl_file.h
@@ -24,14 +24,20 @@ extern int file_get_line_length(FILE* file, int line_n);
 //! \returns The extracted string.
 extern char* file_get_line_str(FILE* file, int line_n);
 
-extern int file_set_line_str(FILE* file, int line_n, char* str);
-
-extern int file_put_line_str(FILE* file, int line_n, char* str);
-
 //! \brief Extract an integer value from a specific line of a given file.
 //! \param file The FILE* to extract an int from.
 //! \param line_n The line number.
 //! \returns the int value if successful, 0 if 0, or if unable to aquire int.
 extern int file_get_line_int(FILE* file, int line_n);
+
+extern int file_put_line_str(FILE* file, int line_n, char* str);
+
+// The following functions have been temporarily delayed as they require
+// more dynamic file stream manipulation. 
+// #include <unistd.h>
+// Consider using ftruncate() to alter the file stream.
+// extern int file_remove_line(FILE* file, int line_n, char* str);
+// extern int file_clear_line(FILE* file, int line_n, char* str);
+// extern int file_set_line_str(FILE* file, int line_n, char* str);
 
 #endif
