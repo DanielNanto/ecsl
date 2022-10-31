888c93d835ed5cf1ab3c24444c224eeb80a1da27
Dan <nanto.daniel@gmail.com>
2022-10-31 07:57:04 -0500

    Un-initialized n_previous to satisfy cppcheck error.

--- ecsl_str.c
+++ ecsl_str.c
@@ -113,7 +113,7 @@ char* int_to_str(int n)
 int str_to_int(char* str)
 {
   int n = 0;
-  int n_previous = 0;
+  int n_previous;
   int length = strlen(str);
   int i;
   for (i = 0; i < length; i++)
