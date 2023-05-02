0523ab1ff553ab87a042ea7570a430247dc35bcd
DanielNanto <nanto.daniel@gmail.com>
2023-05-02 16:51:57 -0500

    Added mainpage documentation for Doxygen.

--- include/ecsl.h
+++ include/ecsl.h
@@ -9,27 +9,33 @@
 //! Date: 2022.8.10 (ISO 8601)  
 //! Standard: JSF-AV C++ | Google C++  
 //! Notes: These are some functions I use frequently enough to collect in one place  
-//! \section Dependencies Non-standard dependencies:
-//! N/A
 //! \section Examples Example Cmake project:
 //! \subsection advice_1 I: Directory layout
-//! Project_folder  
-//!   Software    
-//!     Project_software  
-//!       Build  
-//!       Cmake  
-//!       Data  
-//!       Dev  
-//!       Doc  
-//!       External  
-//!         Etc  
-//!       Include  
-//!       Src  
-//! \subsection advice_2 II: Cmake
-//! set_target_properties(${PROJECT_NAME} PROPERTIES PUBLIC_HEADER include/ecsl.h)  
-//! add_subdirectory(external/ecsl)  
-//! target_link_libraries(${PROJECT_NAME} ecsl)  
-//! \version 1.0.0  
+//! <ol type="I">
+//!   <li>Project_folder
+//!     <ol type="A">
+//!       <li>build</li>
+//!       <li>cmake</li>
+//!       <li>data</li>
+//!       <li>dev</li>
+//!       <li>doc</li>
+//!       <li>external</li>
+//!         <ol type="1">
+//!           <li>ecsl</li>
+//!           <li>etc</li>
+//!         </ol>
+//!       <li>include</li>
+//!       <li>src</li>
+//!     </ol>
+//!   </li>
+//! </ol>
+//! \subsection advice_2 II: Cmake functions
+//! <ol type="I">
+//!   <li>set_target_properties(${PROJECT_NAME} PROPERTIES PUBLIC_HEADER include/ecsl.h)</li>
+//! <li>add_subdirectory(external/ecsl)</li>
+//! <li>target_link_libraries(${PROJECT_NAME} ecsl)</li>  
+//!  
+//! \version 0.1.0  
 
 #include "ecsl_int.h"
 #include "ecsl_str.h"
